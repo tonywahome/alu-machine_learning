8d2116f3d006da81a6da310ab39f75f34b55525d
tonywahome <a.wambugu@alustudent.com>
2025-05-22 23:04:22 +0300

    lineup

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 
 def matrix_transpose(matrix):
-  
-    # Get the number of rows and cols
+      # Get the number of rows and cols
     rows = len(matrix)
     cols = len(matrix[0])
 
