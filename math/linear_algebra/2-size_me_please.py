a6dd8c8024b905117b22523907c2eff5e12b9a26
tonywahome <a.wambugu@alustudent.com>
2025-05-22 23:01:46 +0300

    lineup

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def matrix_shape(matrix):
+   
+    shape = []
+
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        matrix = matrix[0]
+
+    return shape
