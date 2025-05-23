5b62b6fb77b85547a30ea98ab39fadb2a8e6ee3f
tonywahome <a.wambugu@alustudent.com>
2025-05-23 12:36:45 +0300

    6

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def cat_arrays(arr1, arr2):
+    """
+    Concatenates two arrays.
+    Args:
+        arr1 (list): First array.
+        arr2 (list): Second array.
+    Returns:
+        list: Concatenated array.
+    """
+
+    return arr1 + arr2
