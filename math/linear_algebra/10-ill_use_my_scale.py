cf58cf6f87413367628d3a1b7d498923173d77bd
tonywahome <a.wambugu@alustudent.com>
2025-05-23 12:43:12 +0300

    fishscale

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+def np_shape(matrix):
+    """
+    Calculates the shape of a numpy array.
+    Args:
+        matrix (numpy.ndarray): Input array of any dimensionality.
+    Returns:
+        tuple: A tuple representing the shape of the array.
+    """
+    return matrix.shape
