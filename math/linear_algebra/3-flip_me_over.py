ccb57386fac27ac9613e22996fb5d820a9d71c29
tonywahome <a.wambugu@alustudent.com>
2025-05-24 11:20:56 +0100

    n

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
+"""
+Calculates the transpose of a matrix.
+"""
+
+
 def matrix_transpose(matrix):
-      # Get the number of rows and cols
+    """
+    Transposes a matrix.
+
+    """
+    # Get the number of rows and cols
     rows = len(matrix)
     cols = len(matrix[0])
+
     # Create a new matri to create a new transpose
     transposed = []
+
     for col in range(cols):
         new_row = []
         for row in range(rows):
--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+"""
+Adds two arrays element-wise.
+"""
+
+
 def add_arrays(arr1, arr2):
     """
     Adds two arrays element-wise.
