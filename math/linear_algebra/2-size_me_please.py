9831d24ed2e76040e30fd5cc94c4249bf500e0e0
tonywahome <a.wambugu@alustudent.com>
2025-05-24 11:17:46 +0100

    sss

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
+"""
+Calculates the shape of a matrix.
+"""
 
-def matrix_shape(matrix): 
+
+def matrix_shape(matrix):
+    """
+    Calculates the shape of a matrix.
+    Args:
+        matrix (list): A nested list representing a matrix.
+    Returns:
+        list: A list representing the shape of the matrix.
+    """
     shape = []
 
     while isinstance(matrix, list):
--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
-
 def matrix_transpose(matrix):
       # Get the number of rows and cols
     rows = len(matrix)
     cols = len(matrix[0])
-
     # Create a new matri to create a new transpose
     transposed = []
-
     for col in range(cols):
         new_row = []
         for row in range(rows):
             new_row.append(matrix[row][col])
         transposed.append(new_row)
     return transposed
-
