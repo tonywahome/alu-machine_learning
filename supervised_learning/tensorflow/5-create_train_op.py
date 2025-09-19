266bec9b870e27db91ac6b6ad5f6de77a059acb5
tonywahome <a.wambugu@alustudent.com>
2025-09-19 07:58:40 +0200

    changes to tensorflow

---
+++ supervised_learning/tensorflow/5-create_train_op.py
@@ -0,0 +1,9 @@
+
+#!/usr/bin/env python3
+""" train"""
+import tensorflow as tf
+
+
+def create_train_op(loss, alpha):
+    """ training operation"""
+    return tf.train.GradientDescentOptimizer(alpha).minimize(loss)
